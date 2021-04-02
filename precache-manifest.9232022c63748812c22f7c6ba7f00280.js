d9c7dd1d2eec5efab5ec9073e35306c2f9a647ee
siwakorn21 <siwakornboonmun@gmail.com>
2021-04-03 00:34:46 +0700

    Updates

---
+++ precache-manifest.9232022c63748812c22f7c6ba7f00280.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04cfd4486c39a6eaa535",
+    "url": "/me/static/css/main.8e284eb1.chunk.css"
+  },
+  {
+    "revision": "04cfd4486c39a6eaa535",
+    "url": "/me/static/js/main.04cfd448.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "612b43821a97d19f3088c0d9ea655bcb",
+    "url": "/me/index.html"
+  }
+];
